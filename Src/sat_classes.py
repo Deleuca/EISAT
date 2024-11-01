f0fa8d0a3d6295b9285429aed52b6119b89566b0
Deleuca <ryanmaroun3@gmail.com>
2024-11-01 13:00:02 +0200

    -- added class variable

--- Src/sat_classes.py
+++ Src/sat_classes.py
@@ -1,19 +1,36 @@
+class Variable:
+    def __init__(self, name: str):
+        """
+        Represents a boolean variable as a general term.
+        
+       	:param name: The name of the variable
+       	"""
+       	self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+    	return name
+
 class Literal:
-    def __init__(self, name: str, negated: bool = False):
+    def __init__(self, variable: Variable, negated: bool = False):
         """
         Represents a Boolean literal, which can be negated.
         
-        :param name: The name of the variable (e.g., "x1", "x2").
+        :param variable: The name of the variable (e.g., "x1", "x2").
         :param negated: Whether the literal is negated.
         """
-        self.name = name
+        self.variable = variable
         self.negated = negated
     
-    def __repr__(self):
-        return f"{'¬' if self.negated else ''}{self.name}"
+    def getVariable(self):
+    	return self.variable
+    def isNegated(self):
+    	return self.negated
+    def __str__(self):
+        return f"{'¬' if self.negated else ''}{self.variable.getName()}"
     
 class Clause:
-    def __init__(self, var1: Literal, var2: Literal, var3: Literal):
+    def __init__(self, literal_1: Literal, literal_2: Literal, literal_3: Literal):
         """
         Represents a 3-variable clause in a SAT formula.
         
@@ -21,8 +38,7 @@ class Clause:
         :param var2: Second variable in the clause.
         :param var3: Third variable in the clause.
         """
-        self.variables = [var1, var2, var3]
+        self.literals = [literal_1, literal_2, literal_3]
     
-    def __repr__(self):
-        return f"({self.variables[0]} ∨ {self.variables[1]} ∨ {self.variables[2]})"
-
+    def __str__(self):
+        return f"({self.literals[0]} ∨ {self.literals[1]} ∨ {self.literals[2]})"
