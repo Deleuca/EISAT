6374dd32d70b080e420dbe05ba9f9a452d314981
Deleuca <ryanmaroun3@gmail.com>
2024-11-18 20:26:15 +0200

    -- added getter methods

--- Src/node.py
+++ Src/node.py
@@ -5,6 +5,15 @@ class Node:
         self.clause = clause
         self.iteration = iteration
         self.cluster = cluster
-
+    def getName(self):
+        return self.name
+    def getLiteral(self):
+        return self.literal
+    def getClause(self):
+        return self.clause
+    def getIteration(self):
+        return self.iteration
+    def getCluster(self):
+        return self.cluster
     def __repr__(self):
         return f"{self.clause}: {self.literal}"
