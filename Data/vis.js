e6ad1f2ba753ad42ff6396327ea0029704f52561
ioennes <john.chidiac@lau.edu>
2024-12-11 13:20:45 +0200

    -- expanded color palette

--- Data/vis.js
+++ Data/vis.js
@@ -5,7 +5,8 @@ d3.json("graph.json").then(function (data) {
 
     const color = d3.scaleOrdinal([
         "#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a",
-        "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7"
+        "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7", "#f2a2e8",
+        "#ffcfa5", "#d9c9a5"
     ]);
 
     const selectedNodes = new Set();
